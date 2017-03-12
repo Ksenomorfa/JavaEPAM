a0e66807767feca771f90e59f15c1fd1a9cc2513
Ksenomorfa <black_estrella@mail.ru>
2017-03-12 13:13:56 +0300

    *Idea ide
    Signed-off-by: Ksenomorfa <black_estrella@mail.ru>

--- src/main/java/ru/polinabevad/javarecall/epam/TestPriority.java
+++ src/main/java/ru/polinabevad/javarecall/epam/TestPriority.java
@@ -6,8 +6,16 @@ package ru.polinabevad.javarecall.epam;
 public class TestPriority {
     public static void main(String[] args) {
         int i=3;
-        i = -i++ + i++ + -i;
-        System.out.println("i = " + i);
+        int j=0;
+        int k=0;
+        System.out.println(j=-i++);
+        System.out.println(i);
+        System.out.println(k=i++);
+        System.out.println(i);
+        System.out.println(-i);
+        System.out.println(i);
+        System.out.println(j+k-i);
+
     }
 
 }
